68d49f0e4c5f1c44e79231c82ffdd0c74e13237f
NatChapelle <nathchapelle002@gmail.com>
2022-11-16 12:23:53 +0100

    scroll js 2

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
+const nav = document.getElementById("navbar");
+let lastScroll = 0;
+window.addEventListener("scroll", () => {
+  // console.log(window.scrollY);
+  console.log(lastScroll);
+
+  if (window.scrollY < lastScroll) {
+    nav.style.top = 0;
+  } else {
+    nav.style.top = "-60px";
+  }
+  lastScroll = window.scrollY;
+});
+
 // Créer un événément au scroll
 
-// Cacher la navbar si l'utilisateur comment à descendre et la sortir quand il remonte
+// Cacher la navbar si l'utilisateur commence à descendre et la sortir quand il remonte
 
 // Stocker la valeur du précédent niveau de scroll pour savoir si l'on est monté ou descendu
 
